ee8ef2f9e9424b7ac175f68cbbdd63f888139a6b
Roger Hu <haoxianrogerhu@gmail.com>
2020-07-28 20:37:57 -0700

    indent, margin, basic changes

--- transcribe.py
+++ transcribe.py
@@ -4,7 +4,6 @@ import sys
 if len(sys.argv) == 1:
 	sys.exit(0)
 
-
 def load_pairs(fil: str = 'pairs.txt'):
     with open(fil, 'r') as f:
         contents = f.read().split('\n')
@@ -17,11 +16,20 @@ def load_pairs(fil: str = 'pairs.txt'):
 	            pair[1] = pair[1][:pair[1].index(' %')]
         return pairs
 
-def find(lst, strr):
+def find_index(lst, strr):
 	for i in range(len(lst)):
 		if strr in lst[i]:
 			return i
 	return -1
+
+def find_content(lst, strr, deff=None):
+    for item in lst:
+        if strr in item:
+            ret = item[item.index(strr) + len(strr):]
+            while len(ret) > 0 and ret[-1] == ' ':
+                ret = ret[:-1]
+            return ret
+    return deff
 
 pytex = sys.argv[1]
 print("File to transcribe from:", pytex)
@@ -32,6 +40,7 @@ pairs = load_pairs()
 with open(pytex, 'r') as f:
 	content = f.read()
 
+# Remove comments
 while '%%%' in content:
 	ind1 = content.index('%%%')
 	ind2 = ind1 + 3 + content[ind1 + 3:].index('%%%')
@@ -46,36 +55,58 @@ for i in range(len(lines)):
 
 lines = [x for x in lines if x]
 
-font = '12'
-packages = ['amsmath', 'amssymb', 'amsthm']
+# Set font
+font = find_content(lines, '..font', '12')
 
-font = lines[find(lines, '..font')][7:]
+# Set packages
+packages = ['amsmath', 'amssymb', 'amsthm', 'geometry']
+packages.extend(find_content(lines, '..package', '').split())
 
-packages.extend(lines[find(lines, '..packages')][11:].split())
+# Set page size
+paper = find_content(lines, '..paper', 'letter')
 
-print("font:", font)
-print("packages:", packages)
+# Set page orientation
+orientation = find_content(lines, '..orient', 'portrait')
 
-_start = find(lines, '..begin')
-_end = find(lines, '..end')
+# Set margin
+margin = find_content(lines, '..margin', '1')
+
+# Set indent
+indent = find_content(lines, '..indent', '4')
+
+# Set spacing
+spacing = find_content(lines, '..spacing', '1.5')
+line_spread = 1.3
+if spacing.lower() == 'single' or spacing.lower() == '1':
+    line_spread = 1.0
+elif spacing.lower() == 'double' or spacing.lower() == '2':
+    line_spread = 1.6
+
+_start = find_index(lines, '..begin')
+_end = find_index(lines, '..end')
 if _end < _start:
 	sys.exit(1)
 
 _content = "\n".join(lines[_start + 1: _end])
-#print(_content)
 
 for pair in pairs:
 	_content = _content.replace(pair[0], pair[1])
-
-#print(_content)
 
 output = '\n\\documentclass[{0}pt]{{article}}\n'.format(font)
 output += '\\usepackage{'
 for package in packages[:-1]:
 	output += package + ', '
-output += packages[len(packages) - 1] + '}\n\n'
+output += packages[-1] + '}\n'
+
+output += '\\geometry{{{0}paper, {1}, margin={2}in}}\n'.format(paper, orientation, margin)
+
+output += '\\setlength{{\\parindent}}{{{0}em}}\n'.format(indent)
+
+output += '\\setlength{{\\linespread}}{{{0}}}\n'.format(line_spread)
 
 # Add the stuff regarding creating new commands
+
+output += '\n'
 
 output += "\\begin{document}\n\n"
 
