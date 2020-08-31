32f2812fb34254d5b5ab73c5efc4ee03714d34e7
Roger Hu <haoxianrogerhu@gmail.com>
2020-08-30 19:46:15 -0700

    Removed parameters and fields for Replacer

--- transcribe.py
+++ transcribe.py
@@ -88,7 +88,7 @@ def main():
 
     qed_symbol = find_content(lines, '..qed', '$\\blacksquare$')
 
-    replacer = Replacer(lines, find_content, find_index)
+    replacer = Replacer(lines)
 
     replacer.replace()
 
@@ -183,10 +183,8 @@ def main():
 class Replacer():
 
     # Set up \newtheorem and \newtheorem* commands
-    def __init__(self, lines, find_content, find_index):
+    def __init__(self, lines):
         self.lines = lines
-        self.find_content = find_content
-        self.find_index = find_index
 
     def replace(self):
 
@@ -194,7 +192,7 @@ class Replacer():
         INITIALIZING THEOREMS
         """
         self.all_theorem_names = []
-        _initunnumberedtheorem = self.find_content(self.lines, '..initheorem*', None) # Unnumbered theorems to be defined using ..initheorem*
+        _initunnumberedtheorem = find_content(self.lines, '..initheorem*', None) # Unnumbered theorems to be defined using ..initheorem*
         self.unnumbered_theorems = []
         while _initunnumberedtheorem:
             lst = _initunnumberedtheorem.split()
@@ -204,11 +202,11 @@ class Replacer():
             else:
                 self.unnumbered_theorems.append(lst[0])
                 self.all_theorem_names.append(lst[0])
-                line_to_remove = self.find_index(self.lines, '..initheorem*')
+                line_to_remove = find_index(self.lines, '..initheorem*')
                 self.lines.pop(line_to_remove)
-                _initunnumberedtheorem = self.find_content(self.lines, '..initheorem*', None)
+                _initunnumberedtheorem = find_content(self.lines, '..initheorem*', None)
 
-        _initnumberedtheorem = self.find_content(self.lines, '..initheorem', None) # Numbered theorems to be defined using ..initheorem
+        _initnumberedtheorem = find_content(self.lines, '..initheorem', None) # Numbered theorems to be defined using ..initheorem
         self.numbered_theorems = []
         while _initnumberedtheorem:
             lst = _initnumberedtheorem.split()
@@ -219,49 +217,49 @@ class Replacer():
             else:
                 self.numbered_theorems.append(lst)
                 self.all_theorem_names.append(lst[0])
-                line_to_remove = self.find_index(self.lines, '..initheorem')
+                line_to_remove = find_index(self.lines, '..initheorem')
                 self.lines.pop(line_to_remove)
-                _initnumberedtheorem = self.find_content(self.lines, '..initheorem', None)
+                _initnumberedtheorem = find_content(self.lines, '..initheorem', None)
 
         # Check for ..begin section or ..begin subsection or ..begin subsubsection, etc. 
         for i in range(5):
-            _beginsection = self.find_index(self.lines, '..begin {0}section'.format('sub' * i))
+            _beginsection = find_index(self.lines, '..begin {0}section'.format('sub' * i))
             while _beginsection != -1:
                 text = self.lines[_beginsection][len('..begin {0}section'.format('sub' * i)) + 1:].strip()
                 self.lines[_beginsection] = '\\{0}section{{{1}}}'.format('sub' * i, text)
-                _beginsection = self.find_index(self.lines, '..begin {0}section'.format('sub' * i))
+                _beginsection = find_index(self.lines, '..begin {0}section'.format('sub' * i))
 
         """
         BEGIN AND END THEOREMS
         """
-        _begintheorem = self.find_index(self.lines, '..begin thm')
+        _begintheorem = find_index(self.lines, '..begin thm')
         while _begintheorem != -1:
             text = self.lines[_begintheorem][12:].split()
             if len(text) == 1:
                 self.lines[_begintheorem] = '\\begin{{{0}}}'.format(text[0][0].upper() + text[0][1:])
             else:
                 self.lines[_begintheorem] = '\\begin{{{0}}}[{1}]'.format(text[0][0].upper() + text[0][1:], " ".join(text[1:]))
-            _begintheorem = self.find_index(self.lines, '..begin thm')
+            _begintheorem = find_index(self.lines, '..begin thm')
 
-        _endtheorem = self.find_index(self.lines, '..end thm')
+        _endtheorem = find_index(self.lines, '..end thm')
         while _endtheorem != -1:
             text = self.lines[_endtheorem][10:].split()
             self.lines[_endtheorem] = '\\end{{{0}}}'.format(text[0][0].upper() + text[0][1:])
-            _endtheorem = self.find_index(self.lines, '..end thm')
+            _endtheorem = find_index(self.lines, '..end thm')
 
 
         """
         SAME THING BUT PROOFS
         """
-        _beginproof = self.find_index(self.lines, '..begin proof')
+        _beginproof = find_index(self.lines, '..begin proof')
         while _beginproof != -1:
             self.lines[_beginproof] = '\\begin{proof}'
-            _beginproof = self.find_index(self.lines, '..begin proof')
+            _beginproof = find_index(self.lines, '..begin proof')
 
-        _endproof = self.find_index(self.lines, '..end proof')
+        _endproof = find_index(self.lines, '..end proof')
         while _endproof != -1:
             self.lines[_endproof] = '\\end{proof}'
-            _endproof = self.find_index(self.lines, '..end proof')
+            _endproof = find_index(self.lines, '..end proof')
 
 
         """
